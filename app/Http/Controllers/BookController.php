74393bd31ed71a34096b9a0dafa4e1f1977721c3
Abdullah al shahed <abdullahalshahedkhan@gmail.com>
2025-03-26 10:36:18 +0600

    update

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -44,36 +44,70 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Collection;
+
 
 class BookController extends Controller
 {
 
+    // public function index()
+    // {
+    //     // Fetch a list of books from Open Library (Example: Category "love")
+    //     $response = Http::get('https://openlibrary.org/subjects/love.json?limit=100'); // Example category
+
+    //     if ($response->successful()) {
+    //         $books = $response->json()['works'];
+
+    //         // Correct URL formation: Ensure it uses the OLID format
+    //         foreach ($books as &$book) {
+    //             // Get OLID from the 'key' (e.g., '/works/OLID' => 'OLID')
+    //             $olid =  $book['lending_edition'];
+    //             // $olid = str_replace('/works/', '', $book['key']);
+    //             // Correct URL to open book details using the OLID
+    //             $book['url'] = 'http://10.0.10.59:8003/book/' . $olid;
+    //             // Log::info($book["authors"][0]["name"]);
+    //             // dd($book);
+    //         }
+
+    //     } else {
+    //         $books = [];
+    //     }
+
+    //     return view('books.index', ['books' => $books]);
+    // }
+
     public function index()
-    {
-        // Fetch a list of books from Open Library (Example: Category "love")
-        $response = Http::get('https://openlibrary.org/subjects/love.json?limit=100'); // Example category
+{
+    // Fetch a list of books from Open Library
+    $response = Http::get('https://openlibrary.org/subjects/love.json?limit=100');
 
-        if ($response->successful()) {
-            $books = $response->json()['works'];
+    if ($response->successful()) {
+        $books = $response->json()['works'];
 
-            // Correct URL formation: Ensure it uses the OLID format
-            foreach ($books as &$book) {
-                // Get OLID from the 'key' (e.g., '/works/OLID' => 'OLID')
-                $olid =  $book['lending_edition'];
-                // $olid = str_replace('/works/', '', $book['key']);
-                // Correct URL to open book details using the OLID
-                $book['url'] = 'http://10.0.10.59:8003/book/' . $olid;
-                // Log::info($book["authors"][0]["name"]);
-                // dd($book);
-            }
-
-        } else {
-            $books = [];
+        foreach ($books as &$book) {
+            $olid = $book['lending_edition'];
+            $book['url'] = 'http://10.0.10.59:8003/book/' . $olid;
         }
 
-        return view('books.index', ['books' => $books]);
+        // Pagination logic
+        $currentPage = request()->get('page', 1);
+        $perPage = 10; // Number of books per page
+        $booksCollection = new Collection($books);
+
+        $booksPaginated = new LengthAwarePaginator(
+            $booksCollection->forPage($currentPage, $perPage),
+            $booksCollection->count(),
+            $perPage,
+            $currentPage,
+            ['path' => request()->url()]
+        );
+    } else {
+        $booksPaginated = new LengthAwarePaginator([], 0, 10);
     }
+
+    return view('books.index', ['books' => $booksPaginated]);
+}
 
     // public function show($olid)
     // {
